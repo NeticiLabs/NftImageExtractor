36e3c760b77db606620a54e17c17c862662aaeff
masonhunk <aoronchu@gmail.com>
2022-08-31 17:02:42 +0800

    使用随机UserAgent

--- src/accessors/httpAccessor.js
+++ src/accessors/httpAccessor.js
@@ -1,12 +1,11 @@
 import axios from 'axios';
-import Path from 'path';
 import utils from '../utils.js';
-
-let headers= {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0 Safari/605.1.15'
-}
+import uaPool from 'random-useragent';
 
 async function get(url, options={}) {
+    let headers= {
+        'User-Agent': uaPool.getRandom()
+    };
     return utils.tryUntilSucceed(async ()=>{
         return await axios.get(url, {...options, headers:headers})
     });
